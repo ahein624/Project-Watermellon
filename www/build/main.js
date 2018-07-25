71c2b4e1d0026355595765141e52327829fb5252
Andrew <a.hein624@gmail.com>
2018-07-24 22:13:12 -0400

    some rss edits

--- www/build/main.js
+++ www/build/main.js
@@ -1317,8 +1317,8 @@ var RssService = (function () {
         this.storage = storage;
         this.category = [
             {
-                'title': 'World',
-                'link': 'http://rss.nytimes.com/services/xml/rss/nyt/Americas.xml'
+                'title': 'Events',
+                'link': 'https://westmorelandfair.com/feed/?post_type=ai1ec_event'
             },
             {
                 'title': 'Arts',
@@ -1438,12 +1438,10 @@ var RssService = (function () {
     };
     RssService = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["Injectable"])(),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_0__angular_common_http__["a" /* HttpClient */],
-            __WEBPACK_IMPORTED_MODULE_4__ionic_native_in_app_browser__["a" /* InAppBrowser */],
-            __WEBPACK_IMPORTED_MODULE_6__ionic_native_social_sharing__["a" /* SocialSharing */],
-            __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0__angular_common_http__["a" /* HttpClient */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_common_http__["a" /* HttpClient */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_4__ionic_native_in_app_browser__["a" /* InAppBrowser */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__ionic_native_in_app_browser__["a" /* InAppBrowser */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_6__ionic_native_social_sharing__["a" /* SocialSharing */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_6__ionic_native_social_sharing__["a" /* SocialSharing */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_5__ionic_storage__["b" /* Storage */]) === "function" && _d || Object])
     ], RssService);
     return RssService;
+    var _a, _b, _c, _d;
 }());
 
 //# sourceMappingURL=rss-service.js.map
